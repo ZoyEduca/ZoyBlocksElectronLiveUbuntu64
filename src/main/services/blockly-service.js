9633f297291b5cb2486bde93955b884c3e80d697
Felipe Correia <felipecdbm@gmail.com>
2025-10-21 08:16:20 -0300

    refactor: Adicionar o time.sleep ao dicionario

--- src/main/services/blockly-service.js
+++ src/main/services/blockly-service.js
@@ -85,6 +85,9 @@ async function executarCodigo(codigoPython) {
                 case 'iniciar_zoy':
                     comandoSerial = `<${comando}:${argumentos_comando}>`;
                     break;
+                case 'time.sleep':
+                    comandoSerial = `<${comando}:${argumentos_comando}>`;
+                    break;
                 default:
                     logs.push(`[AVISO] Função desconhecida: ${funcao}`);
             }
