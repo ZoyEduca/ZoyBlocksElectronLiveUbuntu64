cd89198972b735dcf6380093f2e60d6ffb590278
Felipe Correia <felipecdbm@gmail.com>
2025-11-04 11:21:32 -0300

    refactor: Corrigir os blocos de leitura analogica e digital do IR

--- src/assets/blocks/zoy/zoy_steam_blocks/cates/infravermelho.js
+++ src/assets/blocks/zoy/zoy_steam_blocks/cates/infravermelho.js
@@ -1,27 +1,39 @@
 (() => {
   // Cor padrão para blocos IR
-  const COR_IR = "#8E44AD";
+  const COR_BLOCOS = "#8E44AD";
 
   const infravermelho = () => {
     Blockly.defineBlocksWithJsonArray([
       {
         type: "ir_read",
-        message0: "Ler sensor IR no pino %1",
+        message0: "Ler sensor IR no pino %1 entrada: %2",
         args0: [
           {
             type: "field_dropdown",
             name: "PIN",
             options: [
-              ["A0", "A0"],
-              ["A1", "A1"],
+              ["A0/D14", "D14"],
+              ["A4/D18", "D18"],
+              ["A5/D19", "D19"],
               ["D2", "D2"],
-              ["D3", "D3"],
+              ["D9", "D9"],
+              ["D10", "D10"],
+              ["D12", "D12"],
+              ["D13", "D13"],
+            ],
+          },
+          {
+            type: "field_dropdown",
+            name: "MODO",
+            options: [
+              ["INPUT", "INPUT"],
+              ["INPUT_PULLUP", "INPUT_PULLUP"],
             ],
           },
         ],
         output: "Boolean", // retorna True/False
-        colour: COR_IR,
-        tooltip: "Retorna True se o sensor IR detectar obstáculo",
+        colour: COR_BLOCOS,
+        tooltip: "Retorna Verdade/Falso se o sensor IR detectar obstáculo",
         helpUrl: "",
       },
 
@@ -34,15 +46,14 @@
             name: "PIN",
             options: [
               ["A0", "A0"],
-              ["A1", "A1"],
-              ["D2", "D2"],
-              ["D3", "D3"],
+              ["A4", "A4"],
+              ["A5", "A5"],
             ],
           },
         ],
         output: "Number",
-        colour: COR_IR,
-        tooltip: "Retorna o valor digital/analógico do sensor IR",
+        colour: COR_BLOCOS,
+        tooltip: "Retorna o valor analógico da leitura do sensor IR",
         helpUrl: "",
       },
 
@@ -54,10 +65,14 @@
             type: "field_dropdown",
             name: "PIN",
             options: [
-              ["A0", "A0"],
-              ["A1", "A1"],
+              ["A0/D14", "D14"],
+              ["A4/D18", "D18"],
+              ["A5/D19", "D19"],
               ["D2", "D2"],
-              ["D3", "D3"],
+              ["D9", "D9"],
+              ["D10", "D10"],
+              ["D12", "D12"],
+              ["D13", "D13"],
             ],
           },
         ],
@@ -70,23 +85,26 @@
         ],
         previousStatement: null,
         nextStatement: null,
-        colour: COR_IR,
+        colour: COR_BLOCOS,
         tooltip: "Executa ações se o sensor detectar obstáculo",
         helpUrl: "",
       },
     ]);
 
-    // Geração de código Python
-    Blockly.JavaScript.forBlock["ir_read"] = (block) => {
+    // Geração de código Javascript
+    Blockly.JavaScript.forBlock["ir_read"] = function (block) {
       const pin = block.getFieldValue("PIN");
-      const code = `ir_read("${pin}")`;
-      return [code, Blockly.JavaScript.forBlock.ORDER_FUNCTION_CALL];
+      // Adiciona "await" pois é uma função assíncrona
+      const modo = block.getFieldValue("MODO");
+      const code = `await digital_read("DIGITAL_READ","${pin}, ${modo}")`;
+      return [code, Blockly.JavaScript.ORDER_AWAIT];
     };
 
-    Blockly.JavaScript.forBlock["ir_value"] = (block) => {
+    Blockly.JavaScript.forBlock["ir_value"] = function (block) {
       const pin = block.getFieldValue("PIN");
-      const code = `ir_value("${pin}")`;
-      return [code, Blockly.JavaScript.forBlock.ORDER_FUNCTION_CALL];
+      // Adiciona "await" pois é uma função assíncrona
+      const code = `await analog_read("ANALOG_READ", "${pin}")`;
+      return [code, Blockly.JavaScript.ORDER_AWAIT];
     };
 
     Blockly.JavaScript.forBlock["ir_if_detect"] = (block) => {
@@ -100,7 +118,7 @@
   const categoriaInfravermelho = {
     kind: "category",
     name: "Infravermelho",
-    colour: COR_IR,
+    colour: COR_BLOCOS,
     contents: [
       { kind: "block", type: "ir_read" },
       { kind: "block", type: "ir_value" },
