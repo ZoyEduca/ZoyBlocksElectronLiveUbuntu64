ce8048b0703a7991f3728f1a424af9da6675577c
ZoyEduca <zoyeduca1@majela.com.br>
2025-12-21 09:00:18 -0300

    criação de script para iniciar zoyblocks

---
+++ zoy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Navega até a pasta do projeto (usando o novo nome da pasta)
+cd ~/Downloads/ZoyBlocks_Electron_Live_Ubuntu64
+# Garante que as dependências estão ok e inicia
+# Se o seu package.json usa "npm start", faça assim:
+npm start -- --no-sandbox
